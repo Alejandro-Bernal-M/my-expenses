70dff889bb490160aadb9ba8399058f560e01cfa
alejandro <alejandrober96@gmail.com>
2023-07-03 10:40:38 -0500

    Add record validations

--- app/models/record.rb
+++ app/models/record.rb
@@ -1,4 +1,8 @@
 class Record < ApplicationRecord
   belongs_to :user
   has_many :categories
+
+  validates :name, presence: true
+  validates :amount, presence: true
+  validates :user_id, presence: true
 end
