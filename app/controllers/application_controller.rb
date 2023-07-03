6bc9cd1137d9768310e1c5afad5f997857e4d70d
alejandro <alejandrober96@gmail.com>
2023-07-03 16:02:54 -0500

    Routes correction, add sign up and log in links on the root

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,9 @@
 class ApplicationController < ActionController::Base
   before_action :update_allowed_parameters, if: :devise_controller?
 
+  def after_sign_in_path_for(resource)
+    user_categories_path(current_user)
+  end
 
   protected
 
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,8 @@
 class UsersController < ApplicationController
   before_action :authenticate_user!
-  def Show; end
+  def index
+    redirect_to user_path(current_user)
+  end
+
+  def show; end
 end
