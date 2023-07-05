894dc579dd5a639b256efabe115b20b80e5d7929
alejandro <alejandrober96@gmail.com>
2023-07-05 11:20:05 -0500

    Install rspec and tests for the models

---
+++ spec/models/category_spec.rb
@@ -0,0 +1,32 @@
+require 'rails_helper'
+
+RSpec.describe Category, type: :model do
+  let(:user) { User.create(name: 'Test User', email: 'test@test.com', password:'password') }
+  let(:category) { Category.new(name: 'Test Category', icon: 'icons8-aeropuerto-48.png', user: user) }
+
+  it 'is valid with valid attributes' do
+    expect(category).to be_valid
+  end
+
+  it 'is not valid without a name' do
+    category.name = nil
+    expect(category).to_not be_valid
+  end
+
+  it 'is not valid without an icon' do
+    category.icon = nil
+    expect(category).to_not be_valid
+  end
+
+  it 'is associated with a user' do
+    expect(category.user).to eq(user)
+  end
+
+  it 'has many records' do
+    expect(category.records).to be_empty
+  end
+
+  it 'has a list of valid icons' do
+    expect(Category::ICONS).to include('icons8-aeropuerto-48.png')
+  end
+end
---
+++ spec/models/record_spec.rb
@@ -0,0 +1,31 @@
+require 'rails_helper'
+
+RSpec.describe Record, type: :model do
+  let(:author) { User.create(name: 'Test User', email: 'test@test.com', password:'password') }
+  let(:category) { Category.create(name: 'Test Category') }
+  let(:record) { Record.new(name: 'Test Record', amount: 100, author: author, category: category) }
+
+  it 'is valid with valid attributes' do
+    expect(record).to be_valid
+  end
+
+  it 'is not valid without a name' do
+    record.name = nil
+    expect(record).to_not be_valid
+  end
+
+  it 'is not valid without an amount' do
+    record.amount = nil
+    expect(record).to_not be_valid
+  end
+
+  it 'is not valid without an author' do
+    record.author = nil
+    expect(record).to_not be_valid
+  end
+
+  it 'is not valid without a category' do
+    record.category = nil
+    expect(record).to_not be_valid
+  end
+end
